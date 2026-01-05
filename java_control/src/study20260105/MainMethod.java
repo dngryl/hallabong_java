894e671a336207257fcd6ef804285a03ced2e595
DW <DW@DESKTOP-RGQNIA2>
2026-01-05 11:21:33 +0900

    1월 5일 if문 주차요금

--- java_control/src/study20260105/MainMethod.java
+++ java_control/src/study20260105/MainMethod.java
@@ -1,14 +1,31 @@
 package study20260105;
 
+import java.util.Scanner;
+
 public class MainMethod {
 
 	public static void main(String[] args) {
 		
 //		Integer.parseInt ()  정수값으로 변경
 //		Math.floor() 소수점 자르기
-		int num = (int) Math.floor(Math.random()*10)+1; // 1을 더해서 범위를 1~10까지 나오도록
+//		int num = (int) Math.floor(Math.random()*9)+7; // 1을 더해서 범위를 1~10까지 나오도록
+//		
+//		System.out.println(num);
 		
-		System.out.println(num);
+		Scanner sc = new Scanner(System.in);
+	
+		
+		int coin = (int)Math.floor(Math.random()*2)+1;
+		// 1: 앞면, 2: 뒷면
+		System.out.println("동전 앞면, 뒷면 맞추기");
+		System.out.println("1 - 앞면, 2 - 뒷면 \n입력: ");
+		int user = sc.nextInt();
+		
+		if(user == coin)
+			System.out.println("맞추기 성공!");
+		else
+			System.out.println("맞추기 실패!");
+		
 		
 	}
 
