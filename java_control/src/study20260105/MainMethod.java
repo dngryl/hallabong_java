e3a2ce40d098f70412c0b75abddea5225581a864
DW <DW@DESKTOP-RGQNIA2>
2026-01-05 10:53:08 +0900

    1월 5일 if문 주차요금

--- java_control/src/study20260105/MainMethod.java
+++ java_control/src/study20260105/MainMethod.java
@@ -4,6 +4,12 @@ public class MainMethod {
 
 	public static void main(String[] args) {
 		
+//		Integer.parseInt ()  정수값으로 변경
+//		Math.floor() 소수점 자르기
+		int num = (int) Math.floor(Math.random()*10)+1; // 1을 더해서 범위를 1~10까지 나오도록
+		
+		System.out.println(num);
+		
 	}
 
 }
